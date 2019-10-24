657e58eddd41ffabfd23c9d0c94f27401ddc9e19
Ashan Shanaka <ashansjp@gmail.com>
2019-10-24 19:45:59 +0530

    MyFlutterApp class created

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,28 @@ import "package:flutter/material.dart";
 
 void main (){
 
-  runApp(
-    MaterialApp(
-      title: "My Flutter App",
-      home: Material(
-        color: Colors.blueAccent,
-        child: Center(
-          child: Text(
-            "Hello Flutter",
-            textDirection: TextDirection.ltr
-          )
+  runApp(new MyFlutterApp());
+}
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My Flutter App",
+        home: Scaffold(
+            appBar: AppBar(title: Text("My first flutter app")),
+            body: Material(
+                color: Colors.blueAccent,
+                child: Center(
+                    child: Text(
+                      "Hello Flutter",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(color: Colors.white, fontSize: 40.0),
+                    )
+                )
+            )
         )
-      )
-    )
-  );
+    );
+  }
+
 }
