3cb46e631824a651899eeb57ca7a634bb2fe4539
Ashan Shanaka <ashansjp@gmail.com>
2019-10-25 07:42:52 +0530

    Font styles added

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text("Spice Jet",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.italic)),
+                    ),
+                    Expanded(
+                      child: Text("From Mumbai to Bangalore via New Delhi",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w300,
+                              fontStyle: FontStyle.italic)),
+                    )
+                  ],
+                )
+              ],
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
-import 'dart:math';
+import'package:flutter/material.dart';
+import './app_screens/home.dart';
 
-import "package:flutter/material.dart";
-import "./app_screens/first_screen.dart";
+void main(){
 
-
-void main (){
-
-  runApp(new MyFlutterApp());
-}
-
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(title: Text("My first flutter app")),
-            body: FirstScreen()
-        )
-    );
-  }
-
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
